9124609455b96577c86e77e6ee83fe01914d5359
Dev Agrawal <agrawadv@mail.uc.edu>
2024-04-24 20:06:13 -0400

    homepage

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,27 @@
+import { Button } from "@/components/ui/button";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-        <Link to="/employees" className="[&.active]:font-bold">
-          Employees
-        </Link>
-        <Link to="/students" className="[&.active]:font-bold">
-          Students
-        </Link>
+      <div className="flex gap-4 justify-center w-full p-8 border-b border-slate-400">
+        <Button variant="link" className="text-white text-lg">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>
+        </Button>
+        <Button variant="link" className="text-white text-lg">
+          <Link to="/employees" className="[&.active]:font-bold">
+            Employees
+          </Link>
+        </Button>
+        <Button variant="link" className="text-white text-lg">
+          <Link to="/students" className="[&.active]:font-bold">
+            Students
+          </Link>
+        </Button>
       </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { Link, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -6,8 +7,28 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="mx-12">
+      <h1 className="text-3xl m-8">School Time</h1>
+      <div className="flex gap-16 my-8">
+        <div className="md:w-1/2 grid grid-cols-1">
+          <Button
+            variant="ghost"
+            className="text-2xl text-center pt-24 pb-28 border"
+            asChild
+          >
+            <Link to="/employees">Employees</Link>
+          </Button>
+        </div>
+        <div className="md:w-1/2 grid grid-cols-1">
+          <Button
+            variant="ghost"
+            className="text-2xl text-center pt-24 pb-28 border"
+            asChild
+          >
+            <Link to="/students">Students</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
